41e6c1707c81b29bf6cab89cc82d70c4e83cca05
nitin-7781 <unknowngamer931@gmail.com>
2026-05-15 23:25:17 +0530

    basic backend setup

---
+++ Backend/src/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectioninstance = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(
+      `the DataBase is running on ${connectioninstance.connection.host}`,
+    );
+  } catch (error) {
+    console.log(`The is a error while connecting to the datbase ${error}`);
+  }
+};
+
+export default connectDB;
