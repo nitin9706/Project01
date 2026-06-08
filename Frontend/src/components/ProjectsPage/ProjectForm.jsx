e10e44ff49087b97f96dced35c22af689284c91b
nitin-7781 <unknowngamer931@gmail.com>
2026-06-09 01:33:44 +0530

    feat: implement project form with repository URL input and deployment function

--- Frontend/src/components/ProjectsPage/ProjectForm.jsx
+++ Frontend/src/components/ProjectsPage/ProjectForm.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import { createDeployment } from "../../Api/dataGet.js";
+
 export default function ProjectForm() {
+  const [repoUrl, setRepoUrl] = useState("");
+
   const inputClass =
     "w-full rounded-2xl border border-(--border-primary) bg-(--bg-secondary) px-4 py-3 text-(--text-primary) outline-none transition placeholder:text-(--text-muted) focus:border-(--border-accent) focus:ring-2 focus:ring-(--glow-primary)";
 
+  const deploy = () => {
+    // createDeployment(repoUrl);
+    console.log(repoUrl);
+  };
+
   return (
-    <form className="w-full space-y-6">
+    <form
+      className="w-full space-y-6"
+      onSubmit={(e) => {
+        deploy();
+        e.preventDefault();
+      }}
+    >
       <div>
         <label className="mb-2 block text-sm font-medium text-(--text-secondary)">
           GitHub Repository
@@ -12,6 +28,10 @@ export default function ProjectForm() {
           type="text"
           placeholder="github.com/username/repo"
           className={inputClass}
+          value={repoUrl}
+          onChange={(e) => {
+            setRepoUrl(e.target.value);
+          }}
         />
       </div>
 
